cd1b58568398945a91c5b0781ec4fbb4621d06d2
Mark Sapiro <mark@msapiro.net>
2017-05-23 00:04:55 +0000

    Added Q_CLUSTER settings for asynchronous tasks.

--- mailman-suite_project/settings.py
+++ mailman-suite_project/settings.py
@@ -390,6 +390,16 @@ HAYSTACK_CONNECTIONS = {
 }
 
 
+#
+# Asynchronous tasks
+#
+Q_CLUSTER = {
+    'timeout': 300,
+    'save_limit': 100,
+    'orm': 'default',
+}
+
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
